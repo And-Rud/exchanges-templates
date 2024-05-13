56c610b5ce94003081ae1f3d5584b456544615d0
Andrii Rudyk <96918101+And-Rud@users.noreply.github.com>
2024-05-13 15:06:57 +0300

    Update index_okx_spot.js

--- okx/index_okx_spot.js
+++ okx/index_okx_spot.js
@@ -1,11 +1,18 @@
 import axios from "axios";
-let crypto = require("crypto");
+import crypto from "crypto";
+import Big from "big.js";
 
 const httpBase = "https://www.okx.com";
 const apikey = "";
 const apiSecret = "";
-const passPhrase = "1";
-const textToSign = "";
+const passPhrase = "";
+let textToSign = "";
+
+function getTimestamp() {
+  const date = new Date();
+  const formattedDate = date.toISOString().slice(0, -5) + "Z";
+  return formattedDate;
+}
 
 async function request(requestPath, params, method, cursor = false) {
   console.log(params);
@@ -13,6 +20,7 @@ async function request(requestPath, params, method, cursor = false) {
     requestPath += params ? `?${new URLSearchParams(params).toString()}` : "";
     params = {};
   }
+  console.log("requestPath", requestPath);
 
   const url = `${httpBase}${requestPath}`;
   const body = params ? JSON.stringify(params) : "";
@@ -33,36 +41,32 @@ async function request(requestPath, params, method, cursor = false) {
     method: method,
     url: url,
     headers: headers,
-    body: body,
+    data: body,
   };
   if (method === "POST") {
-    config.body = body;
+    config.data = body;
+    console.log();
   }
 
+  let res;
   try {
-    const response = await axios(config);
-    const data = response.data;
+    res = await axios(config);
+    const data = res.data;
+    console.log("data", JSON.stringify(data));
     return {
       statusCode: 200,
       status: "success",
-      content: data.data,
+      data: data.data,
     };
   } catch (e) {
-    console.error(e);
-    return null;
+    console.error(e.response.data);
   }
-}
-
-function getTimestamp() {
-  const date = new Date();
-  const formattedDate = date.toISOString().slice(0, -5) + "Z";
-  return formattedDate;
 }
 
 function signature(timestamp, method, requestPath, body, secretKey) {
-  if (method === "GET") {
-    body = "";
-  }
+  // if (method === "GET") {
+  //   body = "";
+  // }
   const message = `${timestamp}${method.toUpperCase()}${requestPath}${body}`;
   textToSign = message;
   console.log("message", message);
@@ -82,3 +86,146 @@ async function getPriceTicker() {
   const params = { instType: "SPOT" };
   return await request("/api/v5/market/tickers", params, "GET");
 }
+
+async function new_order_limit() {
+  const params = {
+    instId: "XRP-USDT",
+    tdMode: "cash",
+    side: "buy",
+    ordType: "limit",
+    px: "0.2",
+    sz: "30",
+  };
+  return await request("/api/v5/trade/order", params, "POST");
+}
+
+async function new_order_market() {
+  const params = {
+    instId: "XRP-USDT",
+    tdMode: "cash",
+    side: "buy",
+    ordType: "market",
+    sz: "7",
+  };
+  return await request("/api/v5/trade/order", params, "POST");
+}
+
+async function cancel_order() {
+  const params = {
+    instId: "XRP-USDT",
+    ordId: "1445738346510848000",
+  };
+  return await request("/api/v5/trade/cancel-order", params, "POST");
+}
+
+async function cancel_trailingStopOrder() {
+  const params = [
+    {
+      instId: "XRP-USDT",
+      algoId: "710217886954164224",
+    },
+  ];
+  return await request("/api/v5/trade/cancel-algos", params, "POST");
+}
+
+async function get_price() {
+  const params = {
+    instId: "XRP-USDT",
+  };
+  return await request("/api/v5/market/ticker", params, "GET");
+}
+
+async function cancel_openOrders() {
+  const params = [
+    {
+      instId: "XRP-USDT",
+      ordId: "1445827864802922496",
+    },
+  ];
+  return await request("/api/v5/trade/cancel-batch-orders", params, "POST");
+}
+
+async function get_openOrders() {
+  const params = {
+    instType: "SPOT",
+    ordType: "limit",
+  };
+  return await request("/api/v5/trade/orders-pending", params, "get");
+}
+
+async function get_order() {
+  const params = {
+    ordId: "710217886954164224",
+    instId: "XRP-USDT",
+  };
+  return await request("/api/v5/trade/order", params, "GET");
+}
+
+async function get_openOrdersTrailingStop() {
+  const params = {
+    ordType: "move_order_stop",
+  };
+  return await request("/api/v5/trade/orders-algo-pending", params, "get");
+}
+
+async function get_orderTrailingStop() {
+  const params = {
+    algoId: "710221015615541248",
+  };
+  return await request("/api/v5/trade/order-algo", params, "GET");
+}
+
+async function getMyTrades() {
+  const params = {
+    instId: "XRP-USDT",
+  };
+  return await request("/api/v5/market/trades", params, "GET");
+}
+
+async function new_order_tpsl() {
+  const params = {
+    instId: "XRP-USDT",
+    tdMode: "cash",
+    side: "buy",
+    ordType: "market",
+    px: "0.2",
+    sz: "7",
+    tpTriggerPx: "0.4", //trigPrice xrp
+    tpOrdPx: "13", //tp in USDT if -1 that mean market price
+    slTriggerPx: "0.7", //trigPrice xrp
+    slOrdPx: "0.3", //sl in USDT if -1 that mean market price
+  };
+  return await request("/api/v5/trade/order", params, "POST");
+}
+
+async function trailing_limitTradeOrder() {
+  const params = {
+    instId: "XRP-USDT",
+    tdMode: "cash",
+    side: "buy",
+    ordType: "move_order_stop",
+    sz: "7",
+    // tpTriggerPx: "0.7", //trigPrice xrp
+    // tpOrdPx: "13", //tp in USDT if -1 that mean market price
+    // slTriggerPx: "0.7", //trigPrice xrp
+    // slOrdPx: "0.3", //sl in USDT if -1 that mean market price
+    callbackRatio: "0.02", //trailing price in %
+    // callbackSpread: 1, //trailing price in USDT
+    activePx: "0.7", //trigerPrice if empty - marketPrice
+  };
+  return await request("/api/v5/trade/order-algo", params, "POST");
+}
+
+// getPriceTicker();
+// cancel_order();
+// cancel_trailingStopOrder();
+// new_order_limit();
+// cancel_openOrders();
+// get_openOrders();
+// get_order();
+// get_orderTrailingStop();
+// get_openOrdersTrailingStop();
+// get_price();
+// getMyTrades();
+// new_order_tpsl();
+// trailing_limitTradeOrder();
