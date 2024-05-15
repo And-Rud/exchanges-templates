3deeb6a8f49a706cacd9985201950b58eb6b1cf9
Andrii Rudyk <96918101+And-Rud@users.noreply.github.com>
2024-05-15 09:56:03 +0300

    Update index_kucoin_spot.js

--- kucoin/index_kucoin_spot.js
+++ kucoin/index_kucoin_spot.js
@@ -14,48 +14,51 @@ async function http_request(method = "GET", endpoint, data = null) {
 
   console.log(`send ${method} request to ${url}:`);
   const timestamp = new Date().getTime();
+  const passph = crypto
+    .createHmac("sha256", apiSecret)
+    .update(passphrase)
+    .digest("base64");
+  const signString = timestamp + method.toUpperCase() + endpoint + data;
+  console.log("signString", signString);
+  const sign = crypto
+    .createHmac("sha256", apiSecret)
+    .update(signString)
+    .digest("base64");
+  console.log("sign", sign);
 
   const headers = {
     "Content-Type": "application/json",
     "KC-API-KEY": apikey,
+    "KC-API-KEY-VERSION": "2",
     "KC-API-TIMESTAMP": timestamp,
-    "KC-API-PASSPHRASE": passphrase,
-    "KC-API-SIGN": signature(endpoint, data, timestamp, method),
+    "KC-API-PASSPHRASE": passph,
+    "KC-API-SIGN": sign,
   };
   console.log("Headers", headers);
-
-  const requestOptions = {
+  let config = {
     method: method,
     url: url,
     headers: headers,
-    body: data,
   };
+  if (data !== "") {
+    config.data = data;
+  }
+  console.log("config", config);
   try {
-    const res = await axios(requestOptions);
-    return {
-      statusCode: 200,
-      status: "success",
-      data: res.data.data,
-    };
+    const res = await axios(config);
+    console.log("res", res);
+    // return {
+    //   statusCode: 200,
+    //   status: "success",
+    //   data: res.data.data,
+    // };
   } catch (e) {
-    console.log(e);
+    console.log(e.response.data);
   }
 }
 
-function signature(
-  request_path = "",
-  body = "",
-  timestamp = "",
-  method = "GET",
-) {
-  body = Array.isArray(body) ? JSON.stringify(body, null, 0) : body;
-  const what = timestamp + method + request_path + body;
-
-  return crypto.createHmac("sha256", apiSecret).update(what).digest("base64");
-}
-
 async function get_account() {
-  const endpoint = "/api/v1/account-overview?currency=USDT";
+  const endpoint = "/api/v1/accounts?type=trade";
   const body = "";
   const response = await http_request("GET", endpoint, body);
   return response;
@@ -68,5 +71,78 @@ async function getPriceTicker() {
   return response;
 }
 
-// let res = await get_account();
-// console.log(res);
+async function marketOrder() {
+  const endpoint = "/api/v1/orders";
+  const body = {
+    symbol: "XRP-USDT",
+    side: "buy",
+    type: "market",
+    size: "30",
+    clientOid: `${Date.now()}`,
+  };
+  return await http_request("POST", endpoint, JSON.stringify(body));
+}
+
+async function limitOrder() {
+  const endpoint = "/api/v1/orders";
+  const body = {
+    symbol: "XRP-USDT",
+    side: "buy",
+    type: "limit",
+    price: "0.2",
+    size: "30",
+    clientOid: `${Date.now()}`,
+  };
+  return await http_request("POST", endpoint, JSON.stringify(body));
+}
+
+async function cancelOrder() {
+  const params = {
+    symbol: "XRP-USDT",
+    orderId: "664387d90a20f50007e89629",
+  };
+  const body = "";
+  const endpoint = `/api/v1/orders/${params.orderId}?symbol=${params.symbol}`;
+
+  return await http_request("DELETE", endpoint, JSON.stringify(body));
+}
+
+async function cancelAllOrders() {
+  const params = {
+    symbol: "XRP-USDT",
+    tradeType: "TRADE",
+  };
+  const body = "";
+  const endpoint = `/api/v1/orders?symbol=${params.symbol}&tradeType=${params.tradeType}`;
+
+  return await http_request("DELETE", endpoint, JSON.stringify(body));
+}
+
+async function getAllOrders() {
+  const body = "";
+  const endpoint = `/api/v1/orders?status=active`;
+
+  return await http_request("GET", endpoint, body);
+}
+
+async function sltpOrder() {
+  const endpoint = "/api/v3/oco/order";
+  const body = {
+    symbol: "XRP-USDT",
+    side: "buy",
+    stopPrice: "0.7",
+    price: "0.2",
+    size: "30",
+    limitPrice: "0.6",
+    clientOid: `${Date.now()}`,
+  };
+  return await http_request("POST", endpoint, JSON.stringify(body));
+}
+
+// marketOrder();
+// get_account();
+// limitOrder();
+// cancelOrder();
+// cancelAllOrders();
+// getAllOrders();
+// sltpOrder();
