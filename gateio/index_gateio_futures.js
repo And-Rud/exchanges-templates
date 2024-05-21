b8c7d7ffcd6f2855a06f688b4f348112c7913836
Andrii Rudyk <96918101+And-Rud@users.noreply.github.com>
2024-05-21 12:09:49 +0300

    Update index_gateio_futures.js

--- gateio/index_gateio_futures.js
+++ gateio/index_gateio_futures.js
@@ -1,14 +1,14 @@
 import crypto from "crypto";
 import axios from "axios";
 const basePath = "https://api.gateio.ws";
+
 const apikey = "";
-const apiSecret =
-  "";
+const apiSecret = "";
 
 async function request(method, url, queryParameters, payloadString) {
   const timestamp = Math.floor(Date.now() / 1000).toString();
   if (payloadString !== "") {
-    payloadString = `${payloadString}`;
+    payloadString = JSON.stringify(payloadString);
   }
 
   const hashedPayload = crypto
@@ -26,7 +26,7 @@ async function request(method, url, queryParameters, payloadString) {
     hashedPayload +
     "\n" +
     timestamp;
-
+  console.log("signatureString", signatureString);
   let signature = crypto
     .createHmac("sha512", apiSecret)
     .update(signatureString, "utf-8")
@@ -45,19 +45,19 @@ async function request(method, url, queryParameters, payloadString) {
     method: method,
     data: payloadString,
     headers: headers,
-    url: basePath + url,
+    url: basePath + url + (queryParameters ? `?${queryParameters}` : ""),
   };
-
+  console.log("config", config);
   try {
     let res = await axios(config);
-    return {
-      statusCode: 200,
-      status: "success",
-      content: res.data,
-    };
+    console.log("res", res.data);
+    // return {
+    //   statusCode: 200,
+    //   status: "success",
+    //   content: res.data,
+    // };
   } catch (e) {
-    console.log(e);
-    throw new Error(JSON.stringify(e));
+    console.log(e.response.data);
   }
 }
 
@@ -69,7 +69,112 @@ async function account_information() {
   return request(method, url, queryParameters, payloadString);
 }
 
-let res = await get_account();
-// let res = await getPriceTicker();
+async function get_openOrders(j) {
+  const url = `/api/v4/futures/${j.settle}/orders`;
+  const queryParameters = "status=open";
+  const payloadString = "";
+  return request("GET", url, queryParameters, payloadString);
+}
 
-console.log("res", res); //JSON.stringify(res)
+async function get_order(j) {
+  const url = `/api/v4/futures/${j.settle}/orders/${j.orderId}`;
+  const queryParameters = "";
+  const payloadString = "";
+  return request("GET", url, queryParameters, payloadString);
+}
+
+async function get_position(j) {
+  const url = `/api/v4/futures/${j.settle}/positions/${j.symbol}`;
+  const queryParameters = "";
+  const payloadString = "";
+  return request("GET", url, queryParameters, payloadString);
+}
+
+async function cancel_allOpenOrders(j) {
+  const url = `/api/v4/futures/${j.settle}/orders`;
+  const queryParameters = `contract=${j.symbol}`;
+  const payloadString = "";
+  return request("DELETE", url, queryParameters, payloadString);
+}
+
+async function cancel_order(j) {
+  const url = `/api/v4/futures/${j.settle}/orders/${j.orderId}`;
+  const queryParameters = ``;
+  const payloadString = "";
+  return request("DELETE", url, queryParameters, payloadString);
+}
+
+async function set_leverage(j) {
+  //not work!!!!
+  const url = `/api/v4/futures/${j.settle}/positions/${j.symbol}/leverage`;
+  const queryParameters = `leverage=${j.leverage}`;
+  const payloadString = "";
+  return request("POST", url, queryParameters, payloadString);
+}
+
+async function new_limitOrder() {
+  const url = `/api/v4/futures/usdt/orders`;
+  const queryParameters = "";
+  const payloadString = {
+    contract: "XRP_USDT",
+    size: 2, //positive number to make a bid, and negative number to ask
+    iceberg: 0,
+    price: "0.4",
+    tif: "gtc",
+    text: "t-my-custom-id",
+    stp_act: "-",
+  };
+  return request("POST", url, queryParameters, payloadString);
+}
+
+async function new_marketOrder() {
+  const url = `/api/v4/futures/usdt/orders`;
+  const queryParameters = "";
+  const payloadString = {
+    contract: "XRP_USDT",
+    size: 1, //positive number to make a bid, and negative number to ask
+    iceberg: 0,
+    price: "0",
+    tif: "ioc",
+    text: "t-my-custom-id",
+    stp_act: "-",
+  };
+  return request("POST", url, queryParameters, payloadString);
+}
+
+async function new_sltpOrder(j) {
+  //this order dont work!!!
+  const url = `/api/v4/futures/usdt/price_orders`;
+  const queryParameters = "";
+  const payloadString = {
+    initial: {
+      contract: "BTC_USDT",
+      size: 100,
+      price: "0",
+      order_type: "close-long-order",
+      tif: "ioc",
+    },
+    trigger: {
+      strategy_type: 0,
+      price_type: 2,
+      price: "30000",
+      rule: 2, //if 1 - price must be < last price, if 2- price>last price
+      expiration: 86400,
+      tif: "gtc",
+      order_type: "close-long-order",
+    },
+    order_type: "close-long-position",
+  };
+  return request("POST", url, queryParameters, payloadString);
+}
+
+// account_information();
+// set_leverage({ symbol: "XRP_USDT", leverage: 10, settle: "usdt" });
+// new_limitOrder();
+// new_marketOrder();
+// cancel_allOpenOrders({ symbol: "XRP_USDT", settle: "usdt" });
+// cancel_order({ settle: "usdt", orderId: "473885400313" });
+// get_order({ settle: "usdt", orderId: "473885400313" });
+// get_openOrders({ settle: "usdt" });
+// get_position({ symbol: "XRP_USDT", settle: "usdt" });
+// new_sltpOrder();
