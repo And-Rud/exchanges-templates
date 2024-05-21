29d22f0934795c7f4f37ef0c6e9da18e2b85c916
Andrii Rudyk <96918101+And-Rud@users.noreply.github.com>
2024-05-21 17:21:03 +0300

    Update index_huobi_spot.js

--- huobi/index_huobi_spot.js
+++ huobi/index_huobi_spot.js
@@ -68,7 +68,7 @@ function create_request(host, method, url, builder) {
 // Choose either the GET request example or the POST request example in the main method
 // 1. The following is an example of the main method for sending GET requests:
 // const method = 'GET';
-async function http_request(method, endpoint) {
+async function http_request(method, endpoint, body) {
   const timestamp = moment.utc().format("YYYY-MM-DDTHH:mm:ss");
 
   const builder = {
@@ -96,25 +96,28 @@ async function http_request(method, endpoint) {
     method: method,
     url: "https://" + host + request.url,
     headers: request.header,
+    data: body,
   };
   console.log("config", config);
   try {
     let res = await axios(config);
+    console.log("res", res.data);
     return {
       statusCode: 200,
       status: "success",
       data: res.data,
     };
   } catch (e) {
-    console.log(e);
-    throw new Error(e);
+    console.log(1);
   }
 }
 
 async function get_account_id() {
   const endpoint = "/v1/account/accounts";
   const method = "GET";
-  return await http_request(method, endpoint);
+  let a = await http_request(method, endpoint);
+  console.log("a", a);
+  return a;
 }
 
 async function get_account() {
@@ -132,12 +135,129 @@ async function getPriceTicker() {
   return await http_request(method, endpoint);
 }
 
-// let res = await get_account();
-// let res2 = await getPriceTicker();
+async function get_accountInfo() {
+  const endpoint = `/v1/account/accounts`;
+  const method = "GET";
+  return await http_request(method, endpoint);
+}
+async function get_openOrders() {
+  const endpoint = `/v1/order/openOrders`;
+  return await http_request("GET", endpoint);
+}
 
-// let find = res.data.data.list.find((i) => i.balance > 0);
-// console.log("res", find);
-// console.log("res", res2.data.data);
+async function get_order(j) {
+  const endpoint = `/v1/order/orders/${j.orderId}`;
+  return await http_request("GET", endpoint);
+}
+
+async function cancel_order(j) {
+  const endpoint = `/v1/order/orders/${j.orderId}/submitcancel`;
+  const body = {
+    "order-id": `${j.orderId}`,
+    symbol: `${j.symbol}`,
+  };
+  return await http_request("POST", endpoint, body);
+}
+
+async function cancel_openOrders(j) {
+  const endpoint = `/v1/order/orders/batchCancelOpenOrders`;
+  let account_id = await get_account_id();
+  let id = account_id.data.data[0].id;
+  console.log("account_id", account_id, id);
+  const body = {
+    "account-id": `${id}`,
+    symbol: `${j.symbol}`,
+    types: `${j.type}`,
+    side: `${j.side}`,
+    size: `${j.size}`,
+  };
+  return await http_request("POST", endpoint, body);
+}
+
+async function new_limitOrder() {
+  //order amount can`t be lower than 10 USDT
+  let account_id = await get_account_id();
+  let id = account_id.data.data[0].id;
+  console.log("account_id", account_id, id);
+  const endpoint = `/v1/order/orders/place`;
+  const body = {
+    "account-id": `${id}`,
+    amount: "35", //in XRP
+    price: "0.3",
+    source: "spot-api",
+    symbol: "xrpusdt",
+    type: "buy-limit",
+  };
+  return await http_request("POST", endpoint, body);
+}
+
+async function new_marketOrder() {
+  //order amount can`t be lower than 10 USDT
+  let account_id = await get_account_id();
+  let id = account_id.data.data[0].id;
+  console.log("account_id", account_id, id);
+  const endpoint = `/v1/order/orders/place`;
+  const body = {
+    "account-id": `${id}`,
+    amount: "11", //in USDT
+    source: "spot-api",
+    symbol: "xrpusdt",
+    type: "buy-market",
+  };
+  return await http_request("POST", endpoint, body);
+}
+
+async function new_sltpLimitOrder() {
+  //order amount can`t be lower than 10 USDT
+  let account_id = await get_account_id();
+  let id = account_id.data.data[0].id;
+  console.log("account_id", account_id, id);
+  const endpoint = `/v1/order/orders/place`;
+  const body = {
+    "account-id": `${id}`,
+    amount: "35", //in XRP
+    price: "0.3",
+    source: "spot-api",
+    symbol: "xrpusdt",
+    "stop-price": "0.4",
+    type: "buy-stop-limit",
+    operator: "lte", //gte(grate than) or lte
+  };
+  return await http_request("POST", endpoint, body);
+}
+
+async function new_sltpOrder() {
+    //this is limit order
+  //order amount can`t be lower than 10 USDT
+  let account_id = await get_account_id();
+  let id = account_id.data.data[0].id;
+  console.log("account_id", account_id, id);
+  const endpoint = `/v1/order/orders/place`;
+  const body = {
+    "account-id": `${id}`,
+    amount: "35", //in XRP
+    price: "0.5",
+    source: "spot-api", //"spot-api" or "margin-api"
+    symbol: "xrpusdt",
+    "stop-price": "0.4",
+    type: "sell-stop-limit",
+    operator: "lte", //gte(grate than) or lte
+  };
+  return await http_request("POST", endpoint, body);
+}
+
+
+// getPriceTicker();
+// get_account();
+// new_limitOrder();
+// new_marketOrder();
+// get_account_id();
+// get_openOrders();
+// get_order({ orderId: "1076759403931037" });
+// cancel_order({ orderId: "1076759403405677", symbol: "xrpusdt" });
+// cancel_openOrders({ symbol: "xrpusdt", side: "buy", type: "buy-limite", size: 100});
+// new_sltpLimitOrder()
+// new_sltpOrder();
 
 // 2. The following is an example of the main method for sending POST requests:
 // const method = 'POST';
